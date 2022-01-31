8da8b525635966461af03d470e0c4f230c2569f7
miro7923 <miro7923@gmail.com>
2022-01-31 17:44:13 +0900

    쿼리구문에 큰 따옴표 사용 문법

--- _posts/2022-01-31-SQL-use-quotes-in-query.md
+++ _posts/2022-01-31-SQL-use-quotes-in-query.md
@@ -10,12 +10,12 @@ tags:
     - DB
 ---
 # 쿼리구문에 큰 따옴표(" ")가 사용되는 경우
-* 컬럼명 [AS] "Alias" 
+* `컬럼명` [AS] "Alias" 
     * 대소문자 구분
     * 특수문자 포함
     * 공백 포함하는 경우
 <br><br>
-* TO_CHAR(sysdate, 'YYYY-MM-DD "Time" HH24:MI')
+* `TO_CHAR(sysdate, 'YYYY-MM-DD "Time" HH24:MI')`
     * 사용자 형식 내 문자열 포함 시
 
 # => 긴가민가하면 작은 따옴표를 쓰자
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -114,7 +114,7 @@ var store = [{
         "teaser": null
       },{
         "title": "쿼리구문에 큰 따옴표 사용 문법",
-        "excerpt":"쿼리구문에 큰 따옴표(“ “)가 사용되는 경우     컬럼명 [AS] “Alias”            대소문자 구분       특수문자 포함       공백 포함하는 경우              TO_CHAR(sysdate, ‘YYYY-MM-DD “Time” HH24:MI’)            사용자 형식 내 문자열 포함 시           =&gt; 긴가민가하면 작은 따옴표를 쓰자  ","categories": ["Database"],
+        "excerpt":"쿼리구문에 큰 따옴표(“ “)가 사용되는 경우     컬럼명 [AS] “Alias”            대소문자 구분       특수문자 포함       공백 포함하는 경우              TO_CHAR(sysdate, 'YYYY-MM-DD \"Time\" HH24:MI')            사용자 형식 내 문자열 포함 시           =&gt; 긴가민가하면 작은 따옴표를 쓰자  ","categories": ["Database"],
         "tags": ["DB"],
         "url": "/database/SQL-use-quotes-in-query/",
         "teaser": null
