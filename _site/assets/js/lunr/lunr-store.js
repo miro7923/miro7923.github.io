d8934c76b736ddd3465580e8ecb6facee78b4e6c
miro7923 <miro7923@gmail.com>
2022-02-17 15:13:37 +0900

    [BOJ] 10872. 팩토리얼 / 브론즈3 / 10분

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -454,4 +454,10 @@ var store = [{
         "tags": ["CS","Computer Hardware","Control Unit"],
         "url": "/computer%20science/CS-14/",
         "teaser": null
+      },{
+        "title": "Java) BOJ 10872. 팩토리얼",
+        "excerpt":"문제 링크       https://www.acmicpc.net/problem/10872     제한       시간 제한 : 1 초   메모리 제한 : 256 MB     문제       0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.     입력       첫째 줄에 정수 N(0 ≤ N ≤ 12)이 주어진다.     출력       첫째 줄에 N!을 출력한다.      👀 풀이       N팩토리얼이 1부터 N까지의 누적곱이기 때문에 누적합을 구하는 코드에서 곱하기로만 바꿨다. 단 구한 팩토리얼을 저장할 변수가 0부터 시작하면 무슨 수를 곱해도 0이 되어버리기 때문에 1부터 시작해야 하는 것만 주의하면 된다.     결과       시간 : 128 ms   메모리 : 14220 KB     코드      ","categories": ["BOJ"],
+        "tags": ["Algorithm","BOJ","No10872","Math","Java"],
+        "url": "/boj/boj10872-java/",
+        "teaser": null
       }]
